af522181a5097bf669a65323075bc2e616fb5df0
Arthurburkey <Arthurburkey@email.com>
2017-07-19 16:52:58 -0700

    Selection group using radio buttons and image files

---
+++ ChallengeConditionalRadioButton/WebForm1.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeConditionalRadioButton
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            /*
+            if (PencilRadioButton.Checked)
+                ResultLabel.Text = "You selected Pencil";
+            else if (PenRadioButton.Checked)
+                ResultLabel.Text = "You selected Pen";
+            else if (PhoneRadioButton.Checked)
+                ResultLabel.Text = "You selected Phone";
+            else if (TabletRadioButton.Checked)
+                ResultLabel.Text = "You selected Tablet";
+            else
+                ResultLabel.Text = "Please select an option";
+            */
+
+            if (PencilRadioButton.Checked)
+                resultImage.ImageUrl = "pencil.png";
+            else if (PenRadioButton.Checked)
+                resultImage.ImageUrl = "pen.png";
+            else if (PhoneRadioButton.Checked)
+                resultImage.ImageUrl = "phone.png";
+            else if (TabletRadioButton.Checked)
+                resultImage.ImageUrl = "tablet.png";
+            else
+                ResultLabel.Text = "Please select an option";
+        }
+    }
+}
